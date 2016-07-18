564dd36376f931047ec75bb715caefafcfd539b1
Tara <tararee@gmail.com>
2016-07-17 23:30:52 -0400

    Added Strings and Integers

--- js/1.00_integers/1.00_integers.js
+++ js/1.00_integers/1.00_integers.js
@@ -57,5 +57,95 @@ console.log(myCupsOfCoffee);
 //The modulus divides an integer by the following integer
 //and returns the remainder
 
+console.log("Hello, World!");
 
+
+//Integers & Variables
+var x = 1;
+console.log(x);
+
+// Variables Think about it like algebra
+//JK, don't do that, you failed algebra :(
+x = 3;
+var y = 6;
+var z = x + y;
+console.log(z);
+
+//Name variable with camel casing and try not
+//to use names that JS reserves, like function or
+//math or something
+var myAge = 33;
+var birthYear = 1982;
+
+var timesAttackedByMonkeys = 2; 
+
+
+var timesAttackedByMonkeys = (z * timesAttackedByMonkeys);
+
+//Operators
+//Mathy stuff, they're called operators!
+1 + 1;
+2 - 2;
+3 * 3;
+4 / 4;
+
+//Shorthand operator
+myAge += 1;
+
+//same as 
+myAge = myAge + 1;
+//which I think would be a good thing to set on something like, you know, a birthday
+
+//Same thing with
+//subtraction, 
+
+myAge -= 10;
+//following is a string, which we haven't
+//gotten to, but just go with it:
+console.log("In my head I am " + myAge);
+
+
+//So you are just taking your variable (myAge),
+//and setting adding, subtracting, etc. the proceeding
+//number to it.
+
+var myAge = 33;
+myAge += 100;
+myAge = myAge + 100;
+myAge -= 100;
+myAge = myAge - 100;
+console.log(myAge);
+//same same^
+
+myAge *= 9;
+myAge /= 7;
+console.log(myAge);
+
+
+//MODULUS
+//A modulus divides one integer by
+//another, then returns the remainder.
+//For the life of me, I can't figure out why I would use
+//it. 
+//Until I figure that out, it will just remind me
+//of how I got my first D on a test in 6th grade
+//and Mr. Tate let us play dodgeball because
+//he could tell I was really sad and that was just
+//really nice.
+console.log(100 % 5); 
+console.log(10 % 4);
+console.log(10 % 3);
+console.log(22 % 5);
+/* I like to comment
+out all the lines as I go with
+the double-slashes,
+but if I go back later and do it (like to temporarily
+"delete" a section of code to see 
+what disappears or something),I use
+these things for larger blocks of text.
+Maybe there's a name for them, but to me they look like
+a katamari trying to roll up a plank that's too big for it.
+This works in JavaScript and CSS, in html it's <!--
+
+	*/
 
--- js/1.10_strings/1.10_strings.js
+++ js/1.10_strings/1.10_strings.js
@@ -1,6 +1,90 @@
+
 //Strings
 //Strings contain more than one value inside of a variable
+//To the best of my knowledge, you can put more or less anything
+//in them.
 
-var myString = "My new string does not need parentheses."
-var myString = "This is also a string."
+//You put text in quotations
+var myName = "Tara";
+console.log(myName);
+//You put a string of text in quotations
+var myFullName = "Tara Ree Miller"
+console.log(myFullName)
+//you don't put a defined variable into quotations,
+//which is why it wasn't console.log("myFullName")
+//which would have printed "myFullName" instead
+//of Tara Ree Miller
+
+// var password = "Passw0rd$"
+// var birthDate = "March 14";
+// console.log(birthDate);
+// console.log("9" + "10");
+
+
+var myLocation = "on a boat";
+var myAttire = "swim trunks";
+var myAttireTwo = "flippy floppies";
+var myActivity = "flippin' burgers";
+var yourLocation = "Kinko's";
+var yourActivity ="straight flippin' copies";
+
+var comma = ",";
+var period = "."
+
+//Concatentation is adding strings together using
+//a + sign. It's fun. It's hard not to call it concantenation #truefacts
+console.log("I got my " + myAttire + " and my " + myAttireTwo
+ + period + " I'm " + myActivity + comma + " you at " + yourLocation
+ + ", " + yourActivity + " .");
+//notice that you can either use the comma that we added as a variable, or 
+//just throw a comma in some quotation marks. I think the latter is easier,
+//because you can control the spaces around it.
+//var birthPlace = birthCity + comma + birthState;
+//console.log(birthPlac2e);
+
+// Challenge: 
+//Write a string that has two variables. Output I graduated from Carmel High School in 1994.
+
+
+//Here, let's make a variable out of a concatenation
+
+var activity = "Bustin' ";
+var speed = 5;
+var SIUnit = " knots";
+
+//the following is the concatenated variable
+var takeALookAtMe = (activity + speed + SIUnit);
+
+//
+console.log 
+(takeALookAtMe + comma + " wind whippin' in my hair")
+
+
+//Let's concatenate that variable into another
+//variable for fun
+
+
+//Or here's one that is clearer, simpler,
+//and easier to use for data mining
+
+var firstName = "Tara";
+var lastName = "Miller";
+var age = 33;
+var gender= "female";
+
+var personalInfo = firstName + " " + lastName + comma + " " + age + " "
++ comma + gender;
+console.log(personalInfo);
+
+//In summary
+
+var introduction = "Hi, my name is " + firstName + " " + lastName + period +
+" I am " + age + " years old, and I'm " + myLocation + period;
+console.log(introduction)
+
+
+
+
+
+//Come up with something else(if time) Use bank account, temperature, etc.
 
