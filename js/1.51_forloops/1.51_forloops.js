9b1c89284d4d8d2240511a4d1bd3f682fa462986
Tara <tararee@gmail.com>
2016-07-21 17:05:01 -0400

    Added for loops

--- js/1.51_forloops/1.51_forloops.js
+++ js/1.51_forloops/1.51_forloops.js
@@ -1,3 +1,22 @@
-//for(i=0, i<100, i+2){
-//	{"Do this function"}
+//For loops are divided into three sections:
+
+//(the variable i is a convention in for loops/*here you set the variable*//*here you set the conditional*/
+/*and here you set something to execute.)*/
+  for( var i = 1; i <= 10; i++){
+	console.log(i);
 }
+
+//Practice
+for(var i = 0; i <= 55; i+=5){
+	console.log(i);
+}
+
+for (var i = 20; i > 1; i--){
+	console.log(i);
+}
+
+
+for(var i=3;i<30;i+=5){
+	console.log("Add 5: " + i);
+}
+
