9d56ee60caf60368d9557f70594969a1993272f2
Tara <tararee@gmail.com>
2016-07-21 13:05:42 -0400

    Added while loops.

--- js/1.40_function/1.40_function.js
+++ js/1.40_function/1.40_function.js
@@ -101,6 +101,35 @@ function totalCosts(makeup, nails, dress, hair, jewelry){
 }
 totalCosts(300, 80, 5000, 250, 600);
 
+//RETURNS!!
+// A function immediately stops at the point where return is called.
+    function masterArtist(first, last)
+         {
+            var full;
+            full = first + last;
+            return full;
+         }
+         
+         function secondFunction()
+         {
+            var result;
+            result = masterArtist("Thomas", "Kincaide");
+            console.log (result );
+         }
+
+function greeting(){
+	return("Hey");
+}
+function neighbor(){
+	return("Bob");
+}
+function weather(){
+	return("sunny");
+}
+var interaction = greeting() + ", " + neighbor() + "! " +
+"It sure is " + weather() + ".";
+
+console.log(interaction);
 
 //Here's a function that returns my pet's names and breeds.
 function petNameAndBreed(name, breed){
--- js/1.50_whileloops/1.50_whileloops.js
+++ js/1.50_whileloops/1.50_whileloops.js
@@ -1,1 +1,42 @@
-//while this is happening, do that
+//While Loops
+
+//We're setting a variable "age" at 0.
+var age = 0;
+		//Now we'll set a condition:
+		//While the age is less than 100, it will
+		//complete the function that we'll set next.
+while(age < 100) {
+	//Now for the function:
+	age++;
+	//Print to the console
+	console.log(age);
+}
+
+//We can add an extra condition to the function:
+var age = 0;
+while(age < 100) {
+	age++;
+	console.log("I am " + age + ".");
+//we'll set a variable that throws out an
+//extra function if a condition is met:
+//if the variable is 33, it will print a statement:
+	if (age === 33){
+	console.log("How did this happen?");
+	}
+}
+
+
+
+
+//We can add one more condition here, which is
+//else.  
+var clock = 0;
+while(clock < 60){
+	clock+=5;
+	if(clock === 30){
+		console.log(clock + " seconds left");
+	} //so if the variable equals 30, it performs the function above
+	else {
+		console.log(clock + " tick");
+	}//or else it will perform the function above 
+}
