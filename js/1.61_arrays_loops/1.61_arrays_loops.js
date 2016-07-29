f4427dd3af6bcf384c1eb77306f6b37c59f59ff9
Tara <tararee@gmail.com>
2016-07-28 22:30:03 -0400

    Worked on 1.61_arrays_loops

---
+++ js/1.61_arrays_loops/1.61_arrays_loops.js
@@ -0,0 +1,36 @@
+//Loops into an Array!
+var earlyWesAndersonMovies = [ "Bottle Rocket", "Rushmore", "The Royal Tennenbaums", "The Life Aquatic"];
+
+//We need a way to print out a long array. 
+//We use a loop.
+//We call this iterating over an array.
+
+ /*   Same as the for loop
+ here, but instead we loop until
+ the end of the array -> movies.length             
+ */
+for (var i = 0; i < wesAndersonMovies.length; i++){
+
+	console.log(movies[i]);
+	//as long as the .length of the var wesAnderson movies
+	//is greater than the var i, it will loop through and
+	//print them out. It's stupid easy.
+}
+
+
+
+var paintColors = ["alizarin crimson", "cadmium green", "Prussian blue", "flake white"];
+
+for (var i = 0; i < paintColors.length; i++){
+	console.log(paintColors[i]);
+}
+
+var reservoirDogs = ["Mr. Pink", "Mr. White", "Mr. Brown"];
+
+for (var i = 0; i < reservoirDogs.length; i++){
+	console.log(reservoirDogs[i]);
+}
+
+
+
+
