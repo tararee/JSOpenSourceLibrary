9c5201c50bf265e78e0013dffb1548da8edc3a67
Tara <tararee@gmail.com>
2016-07-28 22:03:01 -0400

    Worked on arrays

--- js/1.51_forloops/1.51_forloops.js
+++ js/1.51_forloops/1.51_forloops.js
@@ -20,3 +20,46 @@ for(var i=3;i<30;i+=5){
 	console.log("Add 5: " + i);
 }
 
+// Practice problem from codeschool
+
+// The output should follow this format:
+
+// Generator #1 is off.
+// Generator #2 is on, adding 62 MW, for a total of 62 MW!
+
+// We’ve given you some starting variables to use 
+//in your build. Within your loop, consider the possible 
+//scenarios when building your conditions:
+
+//     The Generator provides 62 MW of power.
+//     The Generator provides 124 MW of power.
+//     The Generator is off.
+var currentGen = 1;
+var totalGen = 19;
+var totalMW = 0;
+while (currentGen <= 4){
+  totalMW = currentGen * 62;
+	console.log("Generator #" + currentGen + " is on, adding 62 MW, for a total of " + totalMW + " MW!");
+      currentGen++;
+}
+for ( currentGen > 4; currentGen <= 19; currentGen++){
+totalMW = totalMW + 124;
+	console.log("Generator #" + currentGen + " is on, adding 124 MW, for a total of " + totalMW + " MW!");
+}
+for (currentGen > 4; currentGen <= 124; currentGen ++){
+// console.log("Generator #" + currentGen + " is on.")
+if (currentGen % 2){
+	console.log("Generator #" + currentGen + " is on.");}
+	else {
+		console.log(null);
+
+}
+}
+
+
+// With a set of complex conditional statements 
+//inside the loop, construct a way to only turn on 
+//even numbered turbines. Remember our power output situation:
+
+//     Generators 1 through 4 produce 62 MW.
+//     Generators 5 through 19 produce 124 MW.
--- js/1.60_arrays/1.60_arrays.js
+++ js/1.60_arrays/1.60_arrays.js
@@ -1,2 +1,37 @@
-// var arrayOfRays {"stingrays" , "rays of light" , "death ray" }
+// Arrays
 //
+// An array is a variable
+// which can hold more than one value at a time.
+//Array Literals
+
+
+var myArray = [];
+myArray.length 
+
+//Array Literals
+
+var emptyArray = [];
+var person = ["Tara", "Miller", 33];
+var beatlesArray = ["John", "George", "Paul", "Ringo"];
+var mixedArray = ["John Locke", 4815162342, (1<2)];
+
+
+//Arrays start at 0
+					//0        1        2
+var cutleryArray = ["fork", "knife", "spoon"];
+
+//Accessing items
+console.log(beatlesArray[1]); //1 item
+console.log(mixedArray); // all items
+
+//.length method
+console.log(mixedArray.length); //Gives the number of items in the array.
+
+
+//Adding to an array - use  .push() ->This is a prebuilt function that needs a paramter.
+beatlesArray.push("Pete Best");
+console.log(beatlesArray); //Now has a new member.
+
+//Taking something away use .pop()- another prebuilt function
+cutleryArray.pop("knife");
+console.log(cutleryArray); //Only has two values now, is safer.
