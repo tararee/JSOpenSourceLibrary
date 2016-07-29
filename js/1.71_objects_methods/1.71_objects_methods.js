ac24b4e22f3c2ebfa887bf2bee1e464daedf1e99
Tara <tararee@gmail.com>
2016-07-28 23:27:55 -0400

    Worked on Objects Inheritance

--- js/1.71_objects_methods/1.71_objects_methods.js
+++ js/1.71_objects_methods/1.71_objects_methods.js
@@ -1,1 +1,36 @@
-//I don't know what this means.
+
+//Methods - functions inside of objects.
+//When we want to add a function to the object, we can create methods.
+//We also need to know about the this keyword. 
+//It's pointing to itself.
+
+var inigoMontoya = {
+	name : "Inigo Montoya",
+	nationality: "Spaniard",
+	whatYouDid: "killed my father",
+	whatYouWillDo: "die",
+
+	//Methods
+	greeting: function(){
+		console.log("My name is " + this.name + ". ");
+	},
+	accusation: function(){
+		console.log("You " + this.whatYouDid + ". ");
+	},
+	warning: function(){
+		console.log("Prepare to " + this.whatYouWillDo + ".");
+	},
+	seeSixFingeredMan: function(){	//call functions inside of functions.
+		this.greeting();
+		this.accusation();
+		this.warning();
+ }
+};
+
+//Like the properties, we access the functions with the dot operator.
+//When calling you need to add () at the end.
+inigoMontoya.greeting();
+inigoMontoya.accusation();
+inigoMontoya.warning();
+//All together now:
+inigoMontoya.seeSixFingeredMan();
--- js/1.72_objects_inheritance/1.72_objects_inheritance.js
+++ js/1.72_objects_inheritance/1.72_objects_inheritance.js
@@ -1,2 +1,29 @@
-//I did not inherit an understanding 
-//of objects inheritance from my parents.
+var inigoMontoya = {
+	name : "Inigo Montoya",
+	nationality: "Spaniard",
+	whatYouDid: "killed my father",
+	whatYouWillDo: "die",
+
+	//Methods
+	greeting: function(){
+		console.log("My name is " + this.name + ". ");
+	},
+	accusation: function(){
+		console.log("You " + this.whatYouDid + ". ");
+	},
+	warning: function(){
+		console.log("Prepare to " + this.whatYouWillDo + ".");
+	},
+	seeSixFingeredMan: function(){	//call functions inside of functions.
+		this.greeting();
+		this.accusation();
+		this.warning();
+ }
+};
+
+//Like the properties, we access the functions with the dot operator.
+//When calling you need to add () at the end.
+inigoMontoya.greeting();
+inigoMontoya.accusation();
+inigoMontoya.warning();
+//All together now:
