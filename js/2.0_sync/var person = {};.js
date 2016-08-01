e4f4b3845fb5bd85b0f6c6bae0953770348eb299
Tara <tararee@gmail.com>
2016-08-01 09:48:33 -0400

    Asynchronous

--- js/2.0_sync/var person = {};.js
+++ js/2.0_sync/var person = {};.js
@@ -2,21 +2,29 @@ var person = {};
 
 var bank = {
 	funds: 0,
-	receiveDepositFrom: function(person){
+	receiveDepositFrom: function(person) {
 		this.funds += person.funds;
-		person.funds = 0;
+		person.funds = 0;	
 	}
 };
 
-console.log(person.funds); //undefined!
+ console.log(person.funds); // undefined!
 
-person.funds = (function work(){
-	return 100;
+(function work(person){
+	var end = Date.now() + 4000; // add 4 seconds
+	while(Date.now() < end) {
+		// zzzzzzzz	
+	}
 
-})();
+	person.funds = 100;
+})(person);
 
-console.log(person.funds); //100
+// person.funds = (function work() {
+// 	return 100;
+// })();
 
-bank.receiveDepositFrom(person); //
+console.log(person.funds); // 100
 
-console.log(person.funds);
+bank.receiveDepositFrom(person);
+
+console.log(person.funds); // 0
