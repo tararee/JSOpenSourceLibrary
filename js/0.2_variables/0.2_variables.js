35cce1343c8abb81b9288d977a8e11dba201eced
Tara <tararee@gmail.com>
2016-08-16 00:01:11 -0400

    Added 0.2_variables

---
+++ js/0.2_variables/0.2_variables.js
@@ -0,0 +1,24 @@
+//VARIABLES
+//defined with var 
+
+var myVariable;
+//cannot start with numbers!
+//end it with a semicolon! It's a statement!
+
+//give variables a value
+myVariable = 100;
+//you can name the value and set the value
+//at the same time:
+
+var myVarTara = "Tara";
+//you can change the value as many times as you want
+
+
+myVarTara = "Tara Ree";
+//and it will update because of the way JS works.
+//DYNAMIC TYPES
+//you can change from one type (a string above) to another
+//(an integer below), unlike other languages.
+myVarTara = 33;
+
+//you really shouldn't though
