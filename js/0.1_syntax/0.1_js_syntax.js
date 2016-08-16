1212c7a613bff9af3508b6df98bb5f440f6f1bb8
Tara <tararee@gmail.com>
2016-08-15 20:58:19 -0400

    Added 0.1_syntax

---
+++ js/0.1_syntax/0.1_js_syntax.js
@@ -0,0 +1,14 @@
+// CASE-SENSITIVITY
+// myFunction(){} cannot be called by myFunction();
+
+// STATEMENTS all end with semi-colons
+// alert(); You can end without one, but if anything came after
+//that function, it would explode
+
+//spaces and whitespace are unimportant
+
+//COMMENTS are /* for a lot of space*/ or //for a line or two
+
+//SYNCHRONICITY
+//Statements run top to bottom
+//
